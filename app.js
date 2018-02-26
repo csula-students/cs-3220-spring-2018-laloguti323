a1a496808c5a0d71a0e60026b58b37ed9115ea70
Eduardo Gutierrez <eduardogutierrez521@gmail.com>
2018-02-25 19:15:21 -0800

    lab2

---
+++ app.js
@@ -0,0 +1,55 @@
+
+// PubSub is single object for publish data to multiple subscribers
+class PubSub {
+    constructor () {
+        this.subscribers = [];
+    
+    }
+
+    // subscribe allows a new subscriber to listen for changes by providing
+    // callback function in the parameter
+    subscribe (fn) {
+      
+        this.subscribers.push(fn);
+    
+    }
+
+    // one can publish any data to subscribers
+    publish (data) {
+
+        this.subscribers.forEach(subscriber => {
+
+            subscriber(data);
+
+        });
+
+      }
+
+     }
+
+const pubSub = new PubSub();
+
+
+let ai_count = document.getElementById("ai_count");
+
+function SUBS () {
+
+    incrementalGame.state.counter++;
+
+    ai_count.innerHTML = "AI " + incrementalGame.state.counter;
+}
+
+//unable to get element by classname , 
+//my button would disappear when i made it to a class instead id
+// i used get elementbyid instead of queryselectorall()
+
+
+var subsAI = document.getElementById("aI_button");
+
+subsAI.addEventListener('click', () => {
+
+    pubSub.publish(window.incrementalGame.state.counter);
+
+}) ;
+pubSub.subscribe(SUBS);
+
